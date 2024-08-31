e51a7a3c724b234e93e325d0bf9954414e4854f3
Vasqs <vasquesgamer16@gmail.com>
2024-08-31 15:35:12 -0300

    Update and rename main.mojo to main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+import os
 import requests
 import pandas as pd
+from keep_alive import keep_alive
+
+keep_alive()
+
+COMMANDS_KEY = os.environ['COMMANDS_KEY']
+STATS_KEY = os.environ['STATS_KEY']
+API_KEY = os.environ['API_KEY']
 
 # Importa planilha do Google Sheets com comandos e respostas
-commands = ('https://docs.google.com/spreadsheets/d/' +
-                   '15-6Hgfd_pxuRLYHJGMIFLx4bjJl4k-iiCDkEYecea-c' +
-                   '/export?gid=0&format=csv')
+commands = ('https://docs.google.com/spreadsheets/d/' + COMMANDS_KEY +
+            '/export?gid=0&format=csv')
 
 # Lê a planilha CSV diretamente da URL
 df = pd.read_csv(commands)
 # Mostra as primeiras linhas do DataFrame (0 linhas para conferir o cabeçalho)
 df.head(0)
 
-
 # Lê estatísticas do Dendê Tool
-stats_url = ('https://docs.google.com/spreadsheets/d/' +
-                   '1Im-0MojG7UlW3W4PzHExYI96IU79FXs52MU_MHZ9OMM' +
-                   '/export?gid=1076143484&format=csv')
-
+stats_url = ('https://docs.google.com/spreadsheets/d/' + STATS_KEY +
+             '/export?gid=1076143484&format=csv')
 # Lê a planilha CSV diretamente da URL
 stats = pd.read_csv(stats_url)
 # Extrai e limpa o texto da célula específica
@@ -36,16 +40,15 @@ apl_ogte = stats.iloc[1, 11]
 apl_ogte = str(apl_ogte).strip()
 
 # Define a URL base para a API do Telegram
-base_url = "https://api.telegram.org/bot7501122668:AAHER_PYTh6QNbIXsa3ig9Kh1SyB1iNRJtk"
+base_url = ('https://api.telegram.org/bot' + API_KEY)
+
 
 # Função para ler mensagens recebidas
 def read_msg(offset):
     # Parâmetros para buscar atualizações a partir do último offset
-    parameters = {
-        "offset" : offset
-    }
+    parameters = {"offset": offset}
     # Faz uma requisição GET para obter atualizações do bot
-    resp = requests.get(base_url + "/getUpdates", data = parameters)
+    resp = requests.get(base_url + "/getUpdates", data=parameters)
     data = resp.json()
     print(data)  # Imprime a resposta da API para debug
 
@@ -56,6 +59,7 @@ def read_msg(offset):
     # Atualiza o offset para a próxima requisição
     if data["result"]:
         return data["result"][-1]["update_id"] + 1
+
 
 # Função para gerar uma resposta automática com base na mensagem recebida
 def auto_answer(message):
@@ -74,6 +78,7 @@ def auto_answer(message):
     else:
         return "Não sei esse comando não pvt, manda Ananda me programar melhor aê"
 
+
 # Função para enviar uma mensagem de volta ao chat no Telegram
 def send_msg(message):
     try:
@@ -82,18 +87,21 @@ def send_msg(message):
             if "text" in msg:
                 text = msg["text"]
                 message_id = msg["message_id"]
+                chat_id = msg["chat"]["id"]  # Pega o chat_id do chat recebido
                 # Gera a resposta automática
                 answer = auto_answer(text)
 
                 # Parâmetros para enviar a resposta
                 parameters = {
-                    # "chat_id": "-4598989403",
+                    "chat_id": chat_id,
                     "text": answer,
                     "reply_to_message_id": message_id
                 }
                 # Faz uma requisição GET para enviar a mensagem
-                resp = requests.get(base_url + "/sendMessage", params=parameters)
-                resp.raise_for_status()  # Levanta uma exceção para status de erro HTTP
+                resp = requests.get(base_url + "/sendMessage",
+                                    params=parameters)
+                resp.raise_for_status(
+                )  # Levanta uma exceção para status de erro HTTP
                 print(resp.text)
             else:
                 print("Mensagem sem texto:", msg)
@@ -106,6 +114,7 @@ def send_msg(message):
             print(f"HTTP error occurred: {http_err}")
     except Exception as err:
         print(f"Other error occurred: {err}")
+
 
 # Offset inicial para começar a ler as mensagens
 offset = 0
