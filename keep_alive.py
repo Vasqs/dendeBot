9790fca3a7105a097100e130dd233f1568a6eb60
Vasqs <vasquesgamer16@gmail.com>
2024-08-31 17:31:48 -0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -4,8 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Alive"
-def run():
-  app.run(host='0.0.0.0.0',port=10000)
 def keep_alive():  
     t = Thread(target=run)
     t.start()
