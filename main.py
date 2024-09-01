5d9020c9241ca2b198038ec1fa5955abaaf02879
Vasqs <vasqsb@gmail.com>
2024-09-01 01:20:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,17 @@ commands_url = f'https://docs.google.com/spreadsheets/d/{COMMANDS_KEY}/export?gi
 stats_url = f'https://docs.google.com/spreadsheets/d/{STATS_KEY}/export?gid=1076143484&format=csv'
 
 def fetch_csv(url):
+    # Faz o download do arquivo CSV da URL fornecida
     response = requests.get(url)
-    response.raise_for_status()
-    return pd.read_csv(io.StringIO(response.content.decode('utf-8')))
+    response.raise_for_status()  # Garante que a solicitação foi bem-sucedida
+    return pd.read_csv(io.StringIO(response.content.decode('utf-8')))  # Lê o conteúdo CSV em um DataFrame do pandas
 
 def fetch_commands():
+    # Obtém o DataFrame contendo os comandos
     return fetch_csv(commands_url)
 
 def fetch_stats():
+    # Obtém o DataFrame contendo as estatísticas e retorna um dicionário com valores específicos
     stats = fetch_csv(stats_url)
     return {
         'apl_total': stats.iloc[1, 5],
@@ -42,6 +45,7 @@ stats = fetch_stats()
 base_url = f'https://api.telegram.org/bot{API_KEY}'
 
 def read_msg(offset):
+    # Lê mensagens da API do Telegram a partir do offset fornecido
     parameters = {"offset": offset}
     resp = requests.get(f'{base_url}/getUpdates', params=parameters)
     resp.raise_for_status()
@@ -55,6 +59,7 @@ def read_msg(offset):
     return data["result"][-1]["update_id"] + 1 if data["result"] else offset
 
 def auto_answer(message):
+    # Gera uma resposta automática para a mensagem recebida
     if not message.startswith('/'):
         return None
 
@@ -73,6 +78,7 @@ def auto_answer(message):
         return "Não sei esse comando não pvt, manda Ananda me programar melhor aê"
 
 def send_msg(message):
+    # Envia uma mensagem de volta ao usuário no Telegram
     try:
         if "message" in message:
             msg = message["message"]
